b7328549060bf5faa1ec7139c80b4db66fde1b08
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-10-27 12:52:31 +0200

    work around torch upstream issue

--- overrides.nix
+++ overrides.nix
@@ -3,6 +3,8 @@
   # make it so torch + cuda libs can find the libs from nvidia-docker
   # see https://github.com/nix-community/dream2nix/issues/698
   torch.env.appendRunpaths = [ "/usr/lib64" ];
+  # seems to be a bug in the torch-cu117 package
+  torch.env.autoPatchelfIgnoreMissingDeps = [ "libnvToolsExt.so.1" ];
   nvidia-cublas-cu11.env.appendRunpaths = [ "/usr/lib64" "$ORIGIN" ];
   nvidia-cudnn-cu11.env.appendRunpaths = [ "/usr/lib64" "$ORIGIN" ];
   nvidia-curand-cu11.env.appendRunpaths = [ "/usr/lib64" "$ORIGIN" ];
