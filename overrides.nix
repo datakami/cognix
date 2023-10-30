871cb68f31e1d451f2701bf14cc4d2321f7193d4
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-10-30 16:19:17 +0100

    overrides: add $ORIGIN to torch runpaths, to find cublas at runtime

--- overrides.nix
+++ overrides.nix
@@ -2,7 +2,7 @@
   ## GPU overrides
   # make it so torch + cuda libs can find the libs from nvidia-docker
   # see https://github.com/nix-community/dream2nix/issues/698
-  torch.env.appendRunpaths = [ "/usr/lib64" ];
+  torch.env.appendRunpaths = [ "/usr/lib64" "$ORIGIN" ];
   # seems to be a bug in the torch-cu117 package
   torch.env.autoPatchelfIgnoreMissingDeps = [ "libnvToolsExt.so.1" ];
   nvidia-cublas-cu11.env.appendRunpaths = [ "/usr/lib64" "$ORIGIN" ];
