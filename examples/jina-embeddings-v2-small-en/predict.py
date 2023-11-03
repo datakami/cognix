0296f97e5b7bdb3a343b16d998e06ca08077e748
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-11-03 15:08:55 +0100

    jina-embeddings-v2: add base64 output, list input

--- examples/jina-embeddings-v2-small-en/predict.py
+++ examples/jina-embeddings-v2-small-en/predict.py
@@ -1,10 +1,18 @@
+from typing import Union, List
 from cog import BasePredictor, Input, BaseModel
 from transformers import AutoModel
 #from subprocess import run
 import torch
+import base64
+
+FORMATS = [
+    ("base64", base64.b64encode),
+    ("array", lambda x: x.tolist()),
+]
+FORMATS_MAP = dict(FORMATS)
 
 class Output(BaseModel):
-    vectors: list[float]
+    vectors: str | list[float]
     text: str
 
 class Predictor(BasePredictor):
@@ -12,13 +20,24 @@ class Predictor(BasePredictor):
         """Load the model into memory"""
         #run(["download-weights"], check=True)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = AutoModel.from_pretrained("jinaai/jina-embeddings-v2-small-en", trust_remote_code=True).to(self.device)
+        self.model = AutoModel.from_pretrained("jinaai/jina-embeddings-v2-small-en", trust_remote_code=True, local_files_only=True).eval().to(self.device)
 
     def predict(
         self,
-        text: str = Input(description="Text string to embed"),
-    ) -> Output:
+        text: Union[str, List[str]] = Input(description="Text string to embed"),
+        output_format: str = Input(
+            description="Format to use in outputs",
+            default=FORMATS[0][0],
+            choices=[k for (k, _v) in FORMATS],
+        ),
+    ) -> list[Output]:
         """Run a single prediction on the model"""
+        map_func = FORMATS_MAP[output_format]
+        if not isinstance(text, list):
+            text = [text]
 
-        embeddings = self.model.encode([text])
-        return Output(vectors=embeddings[0].tolist(), text=text)
+        embeddings = self.model.encode(text)
+        res = []
+        for text_, vectors in zip(text, embeddings):
+            res.append(Output(vectors=map_func(vectors), text=text_))
+        return res
