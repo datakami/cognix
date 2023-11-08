62d952504e98f908117d0079294ad769480838c8
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-11-08 17:52:59 +0100

    pget: 0.0.4 -> 0.0.6

--- pkgs/pget.nix
+++ pkgs/pget.nix
@@ -1,12 +1,12 @@
 { buildGoModule, fetchFromGitHub, lib }:
 buildGoModule (rec {
   pname = "pget";
-  version = "0.0.4";
+  version = "0.0.6";
   src = fetchFromGitHub {
     owner = "replicate";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-u1UMpZGiJK3cwo3E2u8E6b4F7fctQEiMTreWajijYes=";
+    hash = "sha256-z/cTlTK+G18fCN5ht0au2wui+AgajeyOw+pSMgiEEHE=";
   };
   vendorHash = "sha256-y1q/eL5vcpieAy1wo9QxTwMiKjn7QimZMZciEvSG6Zc=";
   ldflags = [
