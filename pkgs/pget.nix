0a80726d4da3de7b4cbbb6e118fd4b107b25e4c0
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-12-19 14:11:36 +0100

    pget: 0.3.1 -> 0.5.4

--- pkgs/pget.nix
+++ pkgs/pget.nix
@@ -1,14 +1,14 @@
-{ buildGoModule, fetchFromGitHub, lib }:
-buildGoModule (rec {
+{ buildGo121Module, fetchFromGitHub, lib }:
+buildGo121Module (rec {
   pname = "pget";
-  version = "0.3.1";
+  version = "0.5.4";
   src = fetchFromGitHub {
     owner = "replicate";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-cS6CCCgrkgWu2wklmoBeHkGdhGuRnV84f0GMEjdT8NM=";
+    hash = "sha256-FWK1tOzj6+A0vYXH35ZdQgLu5BFVBwknw6SlPifHbp4=";
   };
-  vendorHash = "sha256-YGP7BQhOuBriKceUYdcvB6UJZ2KJX+2LNbE3f4GvXCo=";
+  vendorHash = "sha256-A5xfD4ykk34JDt7h976rKJD87KkeQ2rIY2tIfhjdO9g=";
   ldflags = [
     "-X github.com/replicate/pget/pkg/version.Version=${version}"
     # "-X github.com/replicate/pget/pkg/version.CommitHash=${src.rev}"
