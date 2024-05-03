52ee6fbc248886cff175f2487a4cc1e50f8663f1
Yorick van Pelt <yorick@yorickvanpelt.nl>
2024-05-03 18:07:53 +0200

    improve some documentation

--- modules/cog-interface.nix
+++ modules/cog-interface.nix
@@ -13,38 +13,67 @@ in
       };
       python_version = mkOption {
         type = types.enum [ "3.8" "3.9" "3.10" "3.11" "3.12" ];
+        example = "3.11";
+        description = ''
+          The minor version of Python to use.
+        '';
       };
       system_packages = mkOption {
         # strings not supported yet
         type = types.listOf (types.oneOf [types.path types.str]);
         default = [];
+        example = [ "pget" "openmpi" ];
+        description = ''
+          A list of Nix packages to install. A full list of the available packages can be found at
+          https://search.nixos.org/packages .
+          Additionally, [pget](https://github.com/replicate/pget) is also available here, along with any other
+          packages custom added to `cognix.systemPackages`.
+        '';
       };
       python_packages = mkOption {
         type = types.listOf types.str;
         default = [];
+        example = [ "torch" ];
+        description = ''
+          A list of Python packages to install, from all of the specified repositories in
+          `python_extra_index_urls`.
+        '';
       };
       python_snapshot_date = mkOption {
         type = types.nullOr types.str;
         default = null;
+        description = ''
+          When writing the lock file, ignore packages published to repositories after this date.
+        '';
       };
       python_extra_index_urls = mkOption {
         type = types.listOf types.str;
         default = [];
         apply = lib.unique;
+        description = ''
+          Extra python repositories to use.
+        '';
       };
       cog_version = mkOption {
         type = types.str;
         default = "0.9.4";
+        description = "The cog-python version to add to the image.";
       };
     };
     predict = mkOption {
       type = types.str;
       default = "predict.py:Predictor";
+      description = ''
+        The pointer to the Predictor object in your code, which defines how predictions are run on your model.
+      '';
     };
     train = mkOption {
       type = types.nullOr types.str;
       default = null;
       example = "train.py:train";
+      description = ''
+        The pointer to the train function in your code, which defines how your model can be trained.
+      '';
     };
     concurrency = mkOption {
       type = types.int;
@@ -56,12 +85,18 @@ in
     systemPackages = mkOption {
       default = {};
       type = types.attrsOf types.package;
+      description = ''
+        Packages to make available for `cog.build.system_packages`.
+      '';
     };
     sourceIgnores = mkOption {
       type = types.lines;
       default = "";
       example = ''
         /models/*
+      '';
+      description = ''
+        gitignore syntax, don't copy these files to the /src package in the image
       '';
     };
     rootPath = mkOption {
@@ -87,9 +122,15 @@ in
     environment = mkOption {
       type = types.attrsOf types.anything;
       default = {};
+      description = ''
+        Set these environment variables in the image.
+      '';
     };
   };
   options.python-env = with lib; mkOption {
+    description = ''
+      Sub-module containing the python environment that's used in the image.
+    '';
     type = types.submoduleWith {
       modules = [ dream2nix.modules.dream2nix.core ];
       specialArgs = { inherit packageSets dream2nix; };
