69bbc898ca1ce4692b2a0a7feb259b87f9def4f6
Yorick van Pelt <yorick@yorickvanpelt.nl>
2024-06-07 17:43:31 +0200

    pget: 0.6.2 -> 0.8.1

--- pkgs/pget.nix
+++ pkgs/pget.nix
@@ -1,16 +1,18 @@
-{ buildGo122Module, fetchFromGitHub, lib }:
+{ buildGo122Module, fetchFromGitHub, stdenv, lib }:
 buildGo122Module (rec {
   pname = "pget";
-  version = "0.6.2";
+  version = "0.8.1";
   src = fetchFromGitHub {
     owner = "replicate";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-vutkaw7UOpGk1LxHBH13SlQ4YdOzHVduVqedoFGhsfw=";
+    hash = "sha256-YWtVP+raAgmMbwbGBKrtDiJ08pV+IjlVe3tKKPm5tCM=";
   };
-  vendorHash = "sha256-U+ZoROoaJuvxExFVmUOBslybx2dEgejwfLs6eJvS91o=";
+  vendorHash = "sha256-L7OJ43MjdclrhIn5orcF1m8b03uiz+jJmoyLY5ltJJU=";
   ldflags = [
     "-X github.com/replicate/pget/pkg/version.Version=${version}"
+    "-X github.com/replicate/pget/pkg/version.OS=${stdenv.targetPlatform.uname.system}"
+    "-X github.com/replicate/pget/pkg/version.Arch=${stdenv.targetPlatform.linuxArch}"
     # "-X github.com/replicate/pget/pkg/version.CommitHash=${src.rev}"
     # not reproducible!:
     # "-X github.com/replicate/pget/pkg/version.BuildTime=$(BUILD_TIME)"
